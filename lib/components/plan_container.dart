816c018118c424d6584fdfef3a11c93fdf17fb30
s0urav-hald3r <bishalder96@gmail.com>
2024-12-10 18:10:05 +0530

    select plan in premium page

--- lib/components/plan_container.dart
+++ lib/components/plan_container.dart
@@ -1,47 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 import 'package:live_flight_tracker/config/colors.dart';
 import 'package:live_flight_tracker/config/extension.dart';
 import 'package:live_flight_tracker/config/icons.dart';
+import 'package:live_flight_tracker/controllers/home_controller.dart';
 
 class PlanContainer extends StatelessWidget {
   const PlanContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = HomeController.instance;
+
     return Container(
       margin: EdgeInsets.symmetric(vertical: 16.h),
       width: MediaQuery.of(context).size.width,
-      child: Column(children: [
-        _planItem(
-          weeklyPlan,
-          '₹369.00/week',
-          'weekly',
-          'weekly',
-          (value) {},
-        ),
-        SizedBox(height: 8.h),
-        _planItem(
-          monthlyPlan,
-          '₹999.00/month',
-          'monthly',
-          'weekly',
-          (value) {},
-        ),
-        SizedBox(height: 8.h),
-        _planItem(
-          yearlyPlan,
-          '₹5,400.00/year',
-          'yearly',
-          'weekly',
-          (value) {},
-        ),
-      ]),
+      child: Obx(() {
+        return Column(children: [
+          _planItem(
+            weeklyPlan,
+            '₹369.00/week',
+            Plan.WEEKLY,
+            controller.selectedPlan,
+          ),
+          SizedBox(height: 8.h),
+          _planItem(
+            monthlyPlan,
+            '₹999.00/month',
+            Plan.MONTHLY,
+            controller.selectedPlan,
+          ),
+          SizedBox(height: 8.h),
+          _planItem(
+            yearlyPlan,
+            '₹5,400.00/year',
+            Plan.YEARLY,
+            controller.selectedPlan,
+          ),
+        ]);
+      }),
     );
   }
 
-  Container _planItem(String icon, String price, String cValue, String gValue,
-      ValueChanged callBack) {
+  Container _planItem(String icon, String price, Plan cValue, Plan gValue) {
     return Container(
       height: 64.h,
       padding: EdgeInsets.symmetric(horizontal: 12.w),
@@ -74,24 +76,30 @@ class PlanContainer extends StatelessWidget {
               ),
             ]),
         const Spacer(),
-        cValue == gValue
-            ? Container(
-                width: 24.w,
-                height: 24.w,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(color: primaryColor, width: 7.5),
-                  color: whiteColor,
-                ),
-              )
-            : Container(
-                width: 24.w,
-                height: 24.w,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(color: whiteColor),
-                ),
-              )
+        if (cValue == gValue)
+          Container(
+            width: 24.w,
+            height: 24.w,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(color: primaryColor, width: 7.5),
+              color: whiteColor,
+            ),
+          )
+        else
+          InkWell(
+            onTap: () {
+              HomeController.instance.selectedPlan = cValue;
+            },
+            child: Container(
+              width: 24.w,
+              height: 24.w,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: whiteColor),
+              ),
+            ),
+          )
       ]),
     );
   }
--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,14 +1,21 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:get/get.dart';
+
+enum Plan { WEEKLY, MONTHLY, YEARLY }
 
 class HomeController extends GetxController {
   static HomeController get instance => Get.find();
 
   // Private variables
   final RxInt _onboardingIndex = 0.obs;
+  final Rx<Plan> _selectedPlan = Plan.WEEKLY.obs;
 
   // Getters
   int get onboardingIndex => _onboardingIndex.value;
+  Plan get selectedPlan => _selectedPlan.value;
 
   // Setters
   set onboardingIndex(value) => _onboardingIndex.value = value;
+  set selectedPlan(value) => _selectedPlan.value = value;
 }
